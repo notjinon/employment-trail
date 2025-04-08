60cb829073d98e68bbc915d497cb5a1a24995f08
Jino Nicolas <onijino22@gmail.com>
2025-04-08 17:35:48 -0400

    chore: Update game.html and gamestyles.css for "The Employment Trail" game

--- gamelogic.js
+++ gamelogic.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function nextSlide() {
+  slides[currentSlide].classList.add("hidden");
+  currentSlide++;
+  slides[currentSlide].classList.remove("hidden");
+}// Your code goes here
